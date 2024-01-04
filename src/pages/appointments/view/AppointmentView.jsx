8ca8a35585bd0c4269ffc748a85468ff8ea05251
jprigotti <julian.nardi@gmail.com>
2024-01-04 19:00:53 -0300

    more progress with the calendar

--- src/pages/appointments/view/AppointmentView.jsx
+++ src/pages/appointments/view/AppointmentView.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./appointmentView.css"
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css';
 
 /*
 Google Appscript WebApp Version Web
@@ -15,24 +17,52 @@ https://script.google.com/macros/s/AKfycbxhO2wz9WJe4N0L9u_nOQLS7UvuXJGTFH44sfNMH
 const AppointmentView = () => {
   const URL_FETCH_GOOGLE = "https://script.google.com/macros/s/AKfycby8J8erqbSvhhfdONhU3FUZwhuocvXn1RL2htG900iXjSxpPkraVKGAxb2ZNix64Auu/exec"
   const GET_EVENT_BY_ID = "getEventByID";
-  const GET_EVENTS_BY_TIME = "getEventsByTime";
+  const GET_EVENTS_BY_TIME_RANGE = "getEventsByTimeRange";
+  const GET_EVENTS_FOR_DAY = "getEventsForDay";
 
-  const handleGetEvents = () => {
+  const today = new Date();
+  const [date, setDate] = useState(today);
+
+
+  const handleGetEventsForDay = (date) => {
+    setDate(date);
+    // Prepara data object with calendar data
+    const request = {
+      type: GET_EVENTS_FOR_DAY,
+      data: {
+        year: date.getFullYear(),
+        month: date.getMonth(),
+        day: date.getDate()
+      }
+    }
+    fetchUrl(request);
+        console.log("Day selected is: ", date.getFullYear(), date.getMonth(), date.getDate())
+  }
+
+  const handleGetEventsByTimeRange = () => {
     // Prepara data object with form input data
-    // const request = {
-    //   type: GET_EVENTS_BY_TIME,
-    //   data: {
-    //     rangeStart: 0,
-    //     rangeEnd: 10
-    //   }
-    // }
+    const request = {
+      type: GET_EVENTS_BY_TIME_RANGE,
+      data: {
+        rangeStart: 0,
+        rangeEnd: 10
+      }
+    }
+    fetchUrl(request);
+  }
+
+
+  const handleGetEventById = (eventId) => {
     const request = {
       type: GET_EVENT_BY_ID,
       data: {
-        event_id: "68niefdb2n4nghuimv1u95ddav@google.com"
+        event_id: eventId
       }
     }
+    fetchUrl(request);
+  }
 
+  const fetchUrl = (request) => {
     // Fetch Gmail to send email
     fetch(URL_FETCH_GOOGLE, {
       method: 'POST',
@@ -44,7 +74,7 @@ const AppointmentView = () => {
       .then((response) => response.json())
       .then((data) => {
         // Handle the response from the Google Apps Script endpoint
-        console.log("Response status: ", data[0].startTime);
+        console.log("Response status: ", data);
         // console.log("Response data: ", data.events);
         // setBtnSubmitText("Enviado!");
         // setTimeout(() => { setBtnSubmitText('Enviar') }, 2000);
@@ -57,13 +87,13 @@ const AppointmentView = () => {
   }
 
 
-
-
-
   return (
     <div>
-      <button onClick={handleGetEvents}>Get Calendar Events</button>
-    </div>
+      <button onClick={() => handleGetEventsByTimeRange()}>Get Calendar Events</button>
+      <button onClick={() => handleGetEventById("28spqq5fj38h0i9shmlev22efm@google.com")}>Get Calendar Events</button>
+
+      <Calendar onChange={handleGetEventsForDay} value={date} />
+    </div >
   )
 }
 
